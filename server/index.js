bf8d1879658ef33608817efd501149a9b1087c49
LouisR17 <roccalouis26@gmail.com>
2026-02-10 13:31:03 +0100

    first commit

---
+++ server/index.js
@@ -0,0 +1,34 @@
+import { Server } from "socket.io";
+
+const io = new Server({
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.join("room1");
+
+  socket.on("leave", () => {
+    socket.disconnect();
+    console.log("a user disconnected");
+  });
+
+  socket.on("chat", (msg) => {
+    console.log("message: " + msg);
+    io.emit("chat", msg);
+  });
+
+  socket.on("join", (pseudo) => {
+    if (!pseudo) return;
+
+    socket.join("room1");
+
+    io.to("room1").emit("join", {
+      pseudo,
+      room: "room1",
+    });
+  });
+});
+
+io.listen(8080);
